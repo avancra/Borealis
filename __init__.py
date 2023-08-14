a8575749aa73470194bed7067e7d2376a13f8b71
Aurélie Vancraeyenest <34453546+avancra@users.noreply.github.com>
2023-08-14 17:08:39 +0300

    Add logging handler to file with DEBUG level

--- __init__.py
+++ __init__.py
@@ -1,17 +1,30 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
+from platformdirs import user_data_dir
+
+
+# default cross-platform directory for Borealis log and config files
+# On windows, it should be something like C:\\Users\\username\\AppData\\Local\\C4XS\\Borealis
+app_name = "Borealis"
+app_author = "C4XS"
+app_dir = Path(user_data_dir(app_name, app_author))
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+# Stream handler to log to the console
+stream_hdlr = logging.StreamHandler()
+stream_hdlr.setLevel(logging.INFO)
+stream_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream_hdlr.setFormatter(stream_formatter)
+logger.addHandler(stream_hdlr)
 
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Handler to file
+file_hdlr = TimedRotatingFileHandler(app_dir / "borealis.log", when="midnight")
+file_hdlr.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_hdlr.setFormatter(stream_formatter)
+logger.addHandler(file_hdlr)
 
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
+logger.debug("\n\n  New Borealis session started \n")
