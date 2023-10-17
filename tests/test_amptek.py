4fcf2e92c9a6a547b2bed0549cc7c5b459ba0903
Aurélie Vancraeyenest <34453546+avancra@users.noreply.github.com>
2023-10-17 17:37:23 +0300

    TEST: add test for Amptek status factory methods

--- tests/test_amptek.py
+++ tests/test_amptek.py
@@ -33,3 +33,27 @@ def test_status(get_raw_status):
     assert status.realtime == pytest.approx(10, abs=0.05)
     assert isinstance(status.fast_count, (float, int))
     assert isinstance(status.slow_count, (float, int))
+
+
+def test_from_status_packet(get_raw_status):
+    status = Status.from_status_packet(get_raw_status)
+    assert isinstance(status.serial_number, str)
+    assert status.serial_number == '022098'
+    assert isinstance(status.realtime, (float, int))
+    assert status.realtime == pytest.approx(10, abs=0.5)
+    assert isinstance(status.acq_time, (float, int))
+    assert status.realtime == pytest.approx(10, abs=0.05)
+    assert isinstance(status.fast_count, (float, int))
+    assert isinstance(status.slow_count, (float, int))
+
+
+def test_from_spectrum_status_packet(get_raw_spectrum_status):
+    status = Status.from_spectrum_status_packet(get_raw_spectrum_status, 2048)
+    assert isinstance(status.serial_number, str)
+    assert status.serial_number == '022098'
+    assert isinstance(status.realtime, (float, int))
+    assert status.realtime == pytest.approx(10, abs=0.5)
+    assert isinstance(status.acq_time, (float, int))
+    assert status.realtime == pytest.approx(10, abs=0.05)
+    assert isinstance(status.fast_count, (float, int))
+    assert isinstance(status.slow_count, (float, int))
