70e687fed7375d05f3fac5bbd299fb6f5b400da1
Aurélie Vancraeyenest <34453546+avancra@users.noreply.github.com>
2023-10-17 17:06:31 +0300

    TEST: add status data and tests

--- tests/test_amptek.py
+++ tests/test_amptek.py
@@ -1,14 +1,30 @@
+import pickle
+from array import array
+from pathlib import Path
+
 import pytest
 
 from borealis.amptek import Status
 
 
-@pytest.mark.xfail(reason="missing status test value")
-def test_status():
-    # TODO: get a real raw_status value
-    raw_status = []
+@pytest.fixture()
+def get_raw_status():
+    with Path('status_data.pickle').open('rb') as fi:
+        statuses = pickle.load(fi)
+    return statuses['raw_status']
+
+
+@pytest.fixture
+def get_raw_spectrum_status():
+    with Path('status_data.pickle').open('rb') as fi:
+        statuses = pickle.load(fi)
+    return statuses['spectrum_status']
+
+
+def test_status(get_raw_status):
+    raw_status = get_raw_status[6:-2]
     status = Status(raw_status)
 
     assert isinstance(status.serial_number, str)
     assert status.serial_number == '022098'
-    assert isinstance(status.acq_time, float)
+    assert isinstance(status.acq_time, (float, int))
