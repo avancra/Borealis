195a55750312cf5f60e44159a8f5f06048094203
Rene Bes <rene.bes@helsinki.fi>
2024-07-29 14:08:21 +0300

    Fix filepath in tests

--- tests/test_amptek.py
+++ tests/test_amptek.py
@@ -8,14 +8,14 @@ from borealis.detector.amptek import Status
 
 @pytest.fixture()
 def get_raw_status():
-    with Path('status_data.pickle').open('rb') as fi:
+    with (Path(__file__).parent /'status_data.pickle').open('rb') as fi:
         statuses = pickle.load(fi)
     return statuses['raw_status']
 
 
 @pytest.fixture
 def get_raw_spectrum_status():
-    with Path('status_data.pickle').open('rb') as fi:
+    with  (Path(__file__).parent /'status_data.pickle').open('rb') as fi:
         statuses = pickle.load(fi)
     return statuses['spectrum_status']
 
