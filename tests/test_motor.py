ceebba0441a29e6077c5e554269ed6a51c0db9a1
Rene Bes <rene.bes@helsinki.fi>
2024-07-30 10:18:17 +0300

    Add docstring in test_motor

--- tests/test_motor.py
+++ tests/test_motor.py
@@ -12,6 +12,10 @@ def test_motor_const():
 
 
 def test_motor_soft_limits():
+    """
+    Check softlimit error is raised when softlimits are reached.
+
+    """
     ctrl = DummyCtrl()
 
     mot = Motor('DummyMotor', '1', 0, ctrl)
@@ -29,15 +33,32 @@ def test_motor_soft_limits():
     mot.set_current_as_zero()
 
 def test_motor_where():
+    """
+    Check the output of the where method.
+
+    The motor is first moved to a position of 10.
+    Position of motor showed in the console output are 10 (user) / 13 (dial).
+
+    """
     ctrl = DummyCtrl()
-    mot = Motor('DummyMotor', '1', 0, ctrl)
+    mot = Motor('DummyMotor', '1', -3, ctrl)
     mot.amove(10)
     mot.where()
 
 def test_motor_scan():
+    """
+    Check the output of the scan method.
+
+    Three tests are performed, (1) without detector and no acquisition time,
+    (2) without a detector but an acquisition time is given,
+    and (3) with a detector and an acquisition time.
+
+    """
     ctrl = DummyCtrl()
     mot = Motor('DummyMotor', '1', 0, ctrl)
     mot.scan(1, 10, 1)
+
     mot.scan(2, 5, .5, acq_time=.1)
+
     det = DummyDet()
     mot.scan(0, 5, 1, acq_time=.5, det=det)
